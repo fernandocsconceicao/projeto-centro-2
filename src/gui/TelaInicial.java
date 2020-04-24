b983ec61e2ad606cd370da676b9fbee477060d30
fernando <fernando@DESKTOP-RGTSFK5>
2020-04-24 03:10:40 -0300

    Texto teste perfeitamente posicionado

--- src/gui/TelaInicial.java
+++ src/gui/TelaInicial.java
@@ -61,6 +61,8 @@ public class TelaInicial extends javax.swing.JFrame {
         jtfNome = new javax.swing.JTextField();
         lblNomeAtr1 = new javax.swing.JLabel();
         lblNomeAtr = new javax.swing.JLabel();
+        lblNomeAtr2 = new javax.swing.JLabel();
+        lblNomeAtr3 = new javax.swing.JLabel();
         jlblBackground = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -78,7 +80,7 @@ public class TelaInicial extends javax.swing.JFrame {
         jbtnImagem.setBounds(400, 590, 180, 50);
 
         jtfNome.setFont(new java.awt.Font("Arial", 0, 36)); // NOI18N
-        jtfNome.setText("321321");
+        jtfNome.setText("Fernando Cesar dos Santos Conceicao");
         jtfNome.setBorder(null);
         jtfNome.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -97,6 +99,15 @@ public class TelaInicial extends javax.swing.JFrame {
         getContentPane().add(lblNomeAtr);
         lblNomeAtr.setBounds(50, 110, 90, 40);
 
+        lblNomeAtr2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagens/Nome_Rect.png"))); // NOI18N
+        getContentPane().add(lblNomeAtr2);
+        lblNomeAtr2.setBounds(150, 190, 780, 62);
+
+        lblNomeAtr3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagens/Nome.png"))); // NOI18N
+        lblNomeAtr3.setText("jLabel1");
+        getContentPane().add(lblNomeAtr3);
+        lblNomeAtr3.setBounds(50, 200, 90, 40);
+
         jlblBackground.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagens/Fundo.png"))); // NOI18N
         getContentPane().add(jlblBackground);
         jlblBackground.setBounds(0, 0, 1045, 684);
@@ -111,24 +122,43 @@ public class TelaInicial extends javax.swing.JFrame {
     private void jbtnImagemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnImagemActionPerformed
         // EN - Definition of the rectangle where the Texts will me placed and centered
         // BR - Definimos aqui retangulo onde o texto sera centralizado futuramente
-        Rectangle nomeRect= new Rectangle(200,270,400,30);
+      
         // EN - Assignment of  the size of the image that will be rendered
         // BR - Definicao das medidas(largura e altura) da imagem a ser renderizada
-        int largura = 842;
-        int altura = 595;
+       
+    
+        
+        
+             
+
+       // int largura =   842;
+      //  int altura =    595;
+        int x_rect,y_rect,ys_rect,xs_rect;
+        int largura =   1123;
+        int altura =    794;
+        x_rect= (int) (largura*0.115);
+        y_rect=  (int)(altura*0.48);
+        xs_rect= (int) (largura*0.78);
+        ys_rect=(int)(altura*0.07);
+        //  Rectangle nomeRect= new Rectangle(200,270,400,30);
+          Rectangle nomeRect= new Rectangle(x_rect,y_rect,xs_rect,ys_rect);
         try{           
             // EN - buffImg is the Bufferedimage that will be printed in the future
             // BR - buffImg e o local onde sera criada uma imagem customizavel. usando as medidas definidas na
             //      largura e altura.
             BufferedImage buffImg= new BufferedImage(largura,altura,BufferedImage.TYPE_INT_ARGB);
+         
             // creation of graphics on BuffImg
             Graphics2D g2d= buffImg.createGraphics();
+            
             // Load images from the directory and store it 
-            BufferedImage fundo = ImageIO.read(new File("imagens/Plano de fundo.png"));
-            BufferedImage texto = ImageIO.read(new File("imagens/Texto certificado.png"));
+            BufferedImage fundo = ImageIO.read(new File("imagens/Plano de fundo 1123x794.png"));
+            //BufferedImage fundo = ImageIO.read(new File("imagens/Plano de fundo.png"));
+            BufferedImage texto = ImageIO.read(new File("imagens/Texto certificado 1123x794.png"));
             
            // Store a font from windows font storage
-            Font font =new Font("Freestyle Script", Font.CENTER_BASELINE, 48);
+            //Font font =new Font("Freestyle Script", Font.CENTER_BASELINE, 48);
+            Font font =new Font("Freestyle Script", Font.CENTER_BASELINE, 53);
             
         
             //  Draw images onto g2d, though its not visible ( bcuz thats not our intention)
@@ -209,5 +239,7 @@ public class TelaInicial extends javax.swing.JFrame {
     private javax.swing.JTextField jtfNome;
     private javax.swing.JLabel lblNomeAtr;
     private javax.swing.JLabel lblNomeAtr1;
+    private javax.swing.JLabel lblNomeAtr2;
+    private javax.swing.JLabel lblNomeAtr3;
     // End of variables declaration//GEN-END:variables
 }
